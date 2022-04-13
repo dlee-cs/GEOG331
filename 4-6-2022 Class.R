12a6a74fc3f70ae0f9c8feaf4c66200029966412
Devon Lee <dslee@colgate.edu>
2022-04-13 12:01:00 -0400

    aligning projections and adding buffers

--- 4-6-2022 Class.R
+++ 4-6-2022 Class.R
@@ -29,3 +29,34 @@ plot(p)
 plotRGB(p, r=3, g=2, b=1,
         scale = 65535,
         stretch = "hist") #increase contrast of image
+
+#read file with field observations of canopy cover
+tree <- read.csv("/Volumes/class/GEOG331_S22/data/rs_data/siberia_stand_data.csv",
+                 header = T)
+#convert to vector object using terra package
+gtree <- vect(tree, geom = c("Long","Lat"), "epsg:4326") #last param = coord syst
+
+#project data to match the coord system of the raster layer
+gtree2 <- project(gtree,p)
+
+#create a polygon from the extent of the points
+#must specify coordinate reference system crs()
+bounding_box <- as.lines(ext(gtree), "epsg:4326")
+
+# plot an rgb rendering of the data
+plotRGB(gtree2, r=3, g=2, b=1,
+        scale = 65535,
+        stretch = "hist")
+
+plot(gtree2, add=T, col = "red")
+
+#reproject the polygons to the same proj as raster
+b2 <-project(bounding_box, crs(p))
+
+#buffer the extent by 200m
+b3 <- buffer(b2, width=200)
+
+plot(b2, add=T)
+plot(b3, add=T)
+
+#use this to crop the raster layer so we can see just our study area
